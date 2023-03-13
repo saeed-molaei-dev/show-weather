a3f2189f45bbac31f779feb4c82877f971da6baf
sam1996 <saeedmolai75@gmail.com>
2023-03-13 15:32:47 +0330

    edit before start developer

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <p>hi man</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
